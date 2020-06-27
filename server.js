e38508c67a32344fd7b1e759629dca6d5341404b
TheMinecrafter05 <59798742+TheMinecrafter05@users.noreply.github.com>
2020-06-27 17:17:41 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+const http = require("http");
+const WebSocket = require("ws");
+const express = require("express");
+
+const port = 3000;
+const server = http.createServer(express)
+const wss = new WebSocket.Server({server});
+
+var messages = []
+
+
+wss.on("connection",ws=>{
+
+    for(i=0;i<messages.length;i++){
+        ws.send(messages[i])
+    }
+
+    ws.on("message",data=>{
+        wss.clients.forEach(client=>{
+            if(client !== ws && client.readyState == WebSocket.OPEN){
+                client.send(data);
+            }
+        })
+
+        if(data == "An user joined!") return;
+
+        messages.push(data)
+    })
+})
+
+server.listen(port,()=>{
+    console.log("Server is on "+port)
+})
